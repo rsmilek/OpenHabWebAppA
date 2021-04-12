18b0b7a9262f8adc6f9dba654ce0af4d83e06c00
Radim Smilek <smilek.radim@gmail.com>
2021-04-12 20:42:12 +0200

    * api endpoint prefix set to: /api/{controller}

--- OpenHabWebAppA/ClientApp/src/main.ts
+++ OpenHabWebAppA/ClientApp/src/main.ts
@@ -8,8 +8,13 @@ export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
 
+export function getBaseApiUrl() {
+  return getBaseUrl() + 'api/';
+}
+
 const providers = [
-  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
+  { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] },
+  { provide: 'BASE_API_URL', useFactory: getBaseApiUrl, deps: [] }
 ];
 
 if (environment.production) {
